cb1ba89f9ba12190412241301cc4d6e6fb8be040
Sofia Gloria Williamson <mushroompup@gmail.com>
2020-09-02 20:34:05 -0500

    Finish Studio 6

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,8 +1,10 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 
 public class BalancedBracketsTest {
 
@@ -11,6 +13,64 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+//    @Test
+//    public void containsBrackets() {
+//
+//    }
+
+//    @Test
+//    public void containsEmptyString() {
+//        assertTrue(!BalancedBrackets.hasBalancedBrackets(""));
+//    }
+
+    @Test public void bracketsWithTextInMiddle() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(("Launchcode")));
+    }
+
+    @Test
+    public void bracketsStartIntheMiddleOfText() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[code]"));
+    }
+
+    @Test
+    public void bracketsFollowedByText() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Launchcode"));
+    }
+
+    @Test public void emptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test public void openBracketFollowedByTextNoClose() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launchcode"));
+    }
+
+    @Test public void closeBracketInTheMiddleOfTextNoOpen() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code"));
+    }
+
+    @Test public void onlyOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test public void onlyClosedBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test public void nestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test public void deepNestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[[]]]]"));
+    }
+
 
 
 }
